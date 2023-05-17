8273911abde61e66fa24b1a714f6964488a39b39
KeilainMan <109026727+KeilainMan@users.noreply.github.com>
2023-05-17 14:01:23 +0200

    Update Singleplayer.gd

--- Singleplayer/Singleplayer.gd
+++ Singleplayer/Singleplayer.gd
@@ -1,25 +1,36 @@
 extends Node
 
 onready var scissors_hand_scene: PackedScene = preload("res://Hands/Scissors.tscn")
+onready var lizard_hand_scene: PackedScene = preload("res://Hands/Lizard.tscn")
+onready var paper_hand_scene: PackedScene = preload("res://Hands/Paper.tscn")
+onready var spock_hand_scene: PackedScene = preload("res://Hands/Spok.tscn")
+onready var stone_hand_scene: PackedScene = preload("res://Hands/Stone.tscn")
 
+enum hands {
+	SCISSORS,
+	STONE,
+	PAPER,
+	LIZARD,
+	SPOK
+}
 
-var scissors := 1
-var stone := 2
-var paper := 3
-var lizard := 4
-var spock := 5
+var enemy_possibilities: Array = [
+	hands.SCISSORS,
+	hands.STONE,
+	hands.PAPER,
+	hands.LIZARD,
+	hands.SPOK,
+]
+
+var current_player_hand: int
+var current_bot_hand: int
+var player_win_counter: int = 0
+var enemy_loose_counter: int = 0
+var enemy_win_counter: int = 0
+var player_loose_counter: int = 0
 
 onready var a_player_1 = get_node("AnimationPlayer")
 onready var a_player_2 = get_node("AnimationPlayer2")
-
-
-var enemy_possibilities := [scissors, stone, paper, lizard, spock]
-var enemy_choice 
-var player_choice
-var player_win_counter :=0
-var enemy_loose_counter :=0
-var enemy_win_counter :=0
-var player_loose_counter :=0
 
 signal stalemate
 signal player_wins
@@ -60,66 +71,66 @@ func hide_ui():
 
 	
 func winner_determination():
-	if player_choice == 1:
-		if enemy_choice ==1:
+	if current_player_hand == hands.SCISSORS:
+		if current_bot_hand == hands.SCISSORS:
 			emit_signal("stalemate") 
 			print("Stalemate")
-		if enemy_choice == 2 or enemy_choice == 5:
+		if current_bot_hand == hands.STONE or current_bot_hand == hands.SPOK:
 			emit_signal("enemy_wins")
 			print("enemy wins")
-		if enemy_choice == 3 or enemy_choice == 4:
+		if current_bot_hand == hands.PAPER or current_bot_hand == hands.LIZARD:
 			emit_signal("player_wins")
 			print("player wins")
-	if player_choice == 2:
-		if enemy_choice == 1 or enemy_choice == 4:
+	if current_player_hand == hands.STONE:
+		if current_bot_hand == hands.SCISSORS or current_bot_hand == hands.LIZARD:
 			emit_signal("player_wins")
-		if enemy_choice == 2:
+		if current_bot_hand == hands.STONE:
 			emit_signal("stalemate")
-		if enemy_choice == 3 or enemy_choice == 5:
+		if current_bot_hand == hands.PAPER or current_bot_hand == hands.SPOK:
 			emit_signal("enemy_wins")
-	if player_choice == 3 :
-		if enemy_choice == 2 or enemy_choice == 5:
+	if current_player_hand == hands.PAPER :
+		if current_bot_hand == hands.STONE or current_bot_hand == hands.SPOK:
 			emit_signal("player_wins")
-		if enemy_choice == 1 or enemy_choice == 4:
+		if current_bot_hand == hands.SCISSORS or current_bot_hand == hands.LIZARD:
 			emit_signal("enemy_wins")
-		if enemy_choice == 3:
+		if current_bot_hand == hands.PAPER:
 			emit_signal("stalemate")
-	if player_choice == 4:
-		if enemy_choice == 3 or enemy_choice == 5:
+	if current_player_hand == hands.LIZARD:
+		if current_bot_hand == hands.PAPER or current_bot_hand == hands.SPOK:
 			emit_signal("player_wins")
-		if enemy_choice == 1 or enemy_choice == 2:
+		if current_bot_hand == hands.SCISSORS or current_bot_hand == hands.STONE:
 			emit_signal("enemy_wins")
-		if enemy_choice == 4:
+		if current_bot_hand == hands.LIZARD:
 			emit_signal("stalemate")
-	if player_choice == 5:
-		if enemy_choice == 1 or enemy_choice == 2:
+	if current_player_hand == hands.SPOK:
+		if current_bot_hand == hands.SCISSORS or current_bot_hand == hands.STONE:
 			emit_signal("player_wins")
-		if enemy_choice == 3 or enemy_choice == 4:
+		if current_bot_hand == hands.PAPER or current_bot_hand == hands.LIZARD:
 			emit_signal("enemy_wins")
-		if enemy_choice == 5:
+		if current_bot_hand == hands.SPOK:
 			emit_signal("stalemate")
 			
 
 
 
 func _on_ASchere_pressed():
-	player_choice = 1
+	current_player_hand = hands.SCISSORS
 	start_after_selection()
 
 func _on_AStein_pressed():
-	player_choice = 2
+	current_player_hand = hands.STONE
 	start_after_selection()
 
 func _on_APapier_pressed():
-	player_choice = 3
+	current_player_hand = hands.PAPER
 	start_after_selection()
 
 func _on_AEchse_pressed():
-	player_choice = 4
+	current_player_hand = hands.LIZARD
 	start_after_selection()
 
 func _on_ASpok_pressed():
-	player_choice = 5
+	current_player_hand = hands.SPOK
 	start_after_selection()
 
 func start_after_selection():
@@ -142,44 +153,44 @@ func _on_Revealtimer_timeout():
 	
 func enemy_choice_selection():
 	enemy_possibilities.shuffle()
-	enemy_choice = enemy_possibilities[0]
+	current_bot_hand = enemy_possibilities[0]
 	show_enemy_choice()
 	
 func show_player_selection():
-	if player_choice == 1:
+	if current_player_hand == hands.SCISSORS:
 		$Hbox/Cntr1/Schere.show()
 		#animation rammen
-	if player_choice == 2:
+	if current_player_hand == hands.STONE:
 		$Hbox/Cntr1/Stein.show()
 		#animation rammen
-	if player_choice == 3:
+	if current_player_hand == hands.PAPER:
 		$Hbox/Cntr1/Papier.show()
 		#animation rammen
-	if player_choice == 4:
+	if current_player_hand == hands.LIZARD:
 		$Hbox/Cntr1/Echse.show()
 		#animation rammen
-	if player_choice == 5:
+	if current_player_hand == hands.SPOK:
 		$Hbox/Cntr1/Spok.show()
 		#animation rammen
 		
 func show_enemy_choice():
-	if enemy_choice == 1:
+	if current_bot_hand == hands.SCISSORS:
 		$Hbox/Cntr2/ESchere.show()
 		#animation rammen
 		winner_determination()
-	if enemy_choice == 2:
+	if current_bot_hand == hands.STONE:
 		$Hbox/Cntr2/EStein.show()
 		#animation rammen
 		winner_determination()
-	if enemy_choice == 3:
+	if current_bot_hand == hands.PAPER:
 		$Hbox/Cntr2/EPapier.show()
 		#animation rammen
 		winner_determination()
-	if enemy_choice == 4:
+	if current_bot_hand == hands.LIZARD:
 		$Hbox/Cntr2/EEchse.show()
 		#animation rammen
 		winner_determination()
-	if enemy_choice == 5:
+	if current_bot_hand == hands.SPOK:
 		$Hbox/Cntr2/ESpok.show()
 		#animation rammen
 		winner_determination()
